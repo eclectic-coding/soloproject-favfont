ca3cd199b1e9f4a531a232fff0cfa14abe5d034a
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-11-11 14:49:52 -0500

    Add fallback to font Data file

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -137,42 +137,52 @@ var fontData = [{
 }, {
   id: 3,
   name: 'Xanh Mono',
+  fallback: 'mono-space',
   designer: 'Lam Bao, Duy Dao, Yellow Type Foundry'
 }, {
   id: 4,
   name: 'Open Sans',
+  fallback: 'san-serif',
   designer: 'Steve Matteson'
 }, {
   id: 5,
   name: 'Big Shoulder Stencil Text',
+  fallback: 'cursive',
   designer: 'Patric King'
 }, {
   id: 6,
   name: 'Noto Sans JP',
+  fallback: 'sans-serif',
   designer: 'Google'
 }, {
   id: 7,
   name: 'Lato',
+  fallback: 'sans-serif',
   designer: 'Łukasz Dziedzic'
 }, {
   id: 8,
   name: 'Source Sans Pro',
+  fallback: 'sans-serif',
   designer: 'Paul D. Hunt'
 }, {
   id: 9,
   name: 'Oswald',
+  fallback: 'sans-serif',
   designer: 'Vernon Adams, Kalapi Gajjar, Cyreal'
 }, {
   id: 10,
   name: 'Poppins',
+  fallback: 'sans-serif',
   designer: 'Indian Type Foundry, Jonny Pinhorn'
 }, {
   id: 11,
   name: 'Raleway',
+  fallback: 'sans-serif',
   designer: 'Multiple Designers'
 }, {
   id: 12,
   name: 'Merriweather',
+  fallback: 'sans-serif',
   designer: 'Sorkin Type'
 }];
 var _default = fontData;
